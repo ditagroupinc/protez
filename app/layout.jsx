6d7b10681142c01b9323ade3215adcba921af015
Pilation <bogdanleontovich@gmail.com>
2023-05-19 18:11:46 +0200

    meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,33 @@ import "./globals.css";
 export const metadata = {
   title: "Prosthetics for Ukraine",
   description: `Main page of the "Prosthetics for Ukraine" foundation`,
+  creator: "Ditagoup",
+  publisher: "Ditagroup",
   openGraph: {
-    url: "http://protezfoundation.com",
-    title: "Prosthetics for Ukraine",
-    description:
-      "Main page of the &quot;Prosthetics for Ukraine&quot; foundation",
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    url: "https://nextjs.org",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://nextjs.org/og.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://nextjs.org/og-alt.png",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en-US",
     type: "website",
+    // url: "http://protezfoundation.com",
+    // title: "Prosthetics for Ukraine",
+    // description:
+    //   "Main page of the &quot;Prosthetics for Ukraine&quot; foundation",
+    // type: "website",
   },
 };
 
