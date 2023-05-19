d24d13c125ab2e19295fb5a37479da116fce76a2
Pilation <bogdanleontovich@gmail.com>
2023-05-19 18:17:11 +0200

    init

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,23 +9,11 @@ export const metadata = {
   creator: "Ditagoup",
   publisher: "Ditagroup",
   openGraph: {
-    title: "Next.js",
-    description: "The React Framework for the Web",
-    url: "https://nextjs.org",
-    siteName: "Next.js",
-    images: [
-      {
-        url: "https://nextjs.org/og.png",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://nextjs.org/og-alt.png",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
+    title: "Prosthetics for Ukraine",
+    description: `Main page of the "Prosthetics for Ukraine" foundation`,
+    url: "http://www.protezfoundation.com/",
+    siteName: "ProtezFoundation.com",
+    images: "/og-image.png",
     locale: "en-US",
     type: "website",
     // url: "http://protezfoundation.com",
