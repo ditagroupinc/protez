c81c3e737093c97909baf392bdbb71c999b2b7d7
Bogdan Leontovych <bogdanleontovich@gmail.com>
2023-07-24 15:52:19 +0200

    google analytics + page fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,13 @@ import "slick-carousel/slick/slick-theme.css";
 import "@/styles/resetCSS.css";
 import "@/styles/fonts.css";
 import "./globals.css";
+import Script from "next/script";
 import LanguageContextProvider from "@/contexts/LanguageContext";
 import ScreenModeAndSizeContextProvider from "@/contexts/ScreenModeAndSizeContext";
+// https://www.rodyvansambeek.com/blog/easiest-ga4-integration-nextjs-13-gtm-guide
+
+const GTM_ID = "GTM-MP2L4TL";
+
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase
 export const metadata = {
   title: "Prosthetics for Ukraine",
@@ -33,12 +38,34 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <LanguageContextProvider>
-      <ScreenModeAndSizeContextProvider>
-        <html lang="en">
-          <body>{children}</body>
-        </html>
-      </ScreenModeAndSizeContextProvider>
-    </LanguageContextProvider>
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+
+      <LanguageContextProvider>
+        <ScreenModeAndSizeContextProvider>
+          <html lang="en">
+            <body>
+              {children}
+              <noscript>
+                <iframe
+                  src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                  height="0"
+                  width="0"
+                  style="display:none;visibility:hidden"
+                ></iframe>
+              </noscript>
+            </body>
+          </html>
+        </ScreenModeAndSizeContextProvider>
+      </LanguageContextProvider>
+    </>
   );
 }
