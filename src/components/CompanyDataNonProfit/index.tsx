5219d33ca1bc3e544891dbf33695d6b9bffb1d73
BohdanLeontovych <bogdanleontovich@gmail.com>
2024-05-10 23:54:05 +0200

    iframe position fix, companyDataNonProfit text fix

--- src/components/CompanyDataNonProfit/index.tsx
+++ src/components/CompanyDataNonProfit/index.tsx
@@ -2,6 +2,7 @@ import { useLanguage } from '@/contexts/LanguageContext'
 import SquareButton from '@/components/SquareButton'
 import Divider from '@/components/Divider'
 import texts from '@/texts&svg'
+import icons from './icons'
 
 import style from './style.module.scss'
 
@@ -36,9 +37,22 @@ const CompanyDataNonProfit = ({
           emptyBlack={black}
           link
           blank
-          href="https://docs.google.com/forms/d/e/1FAIpQLSf_ESrB0vY6973GQSYfDY-WtWYE8UXnaeHJzxIQrEWPaQ_UXw/"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSf_ESrB0vY6973GQSYfDY-WtWYE8UXnaeHJzxIQrEWPaQ_UXw/viewform"
         >
-          {texts.companyData.actionButtons.makeDonation[lang]}
+          {texts.companyData.actionButtons.needAProtez[lang]}
+        </SquareButton>
+
+        <SquareButton
+          emptyBlack={black}
+          link
+          blank
+          href="https://a.co/1DFbVIa?fbclid=IwAR26aMbpS7n8oVD2YlJu6fgSHf9BzRihSkHd19MhB4tNsFRcNLU6-Ja0hFc"
+          className={style.amazonButton}
+        >
+          <span className={style.amazonButtonText}>
+            {texts.companyData.actionButtons.supportWith[lang]}
+          </span>
+          {black ? icons.amazonBlack() : icons.amazon()}
         </SquareButton>
       </div>
     </div>
