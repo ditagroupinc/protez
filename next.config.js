fcd1f283c36fc038243989bca48de4531c27e19a
BohdanLeontovych <bogdanleontovich@gmail.com>
2024-06-04 16:26:01 +0200

    working on images path

--- next.config.js
+++ next.config.js
@@ -5,13 +5,14 @@ const nextConfig = {
   },
   images: {
     domains: ['protez.wpengine.com'],
-    loader: 'custom',
-    loaderFile: './ImageLoader.js',
+    // loader: 'custom',
+    // loaderFile: './ImageLoader.js',
   },
 
   reactStrictMode: false,
   output: 'export',
   basePath: '/protez',
+  assetPrefix: '/protez/',
 }
 
 module.exports = nextConfig
