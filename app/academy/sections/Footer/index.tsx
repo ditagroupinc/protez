1449dc19282edeb282700610605815c785fac285
BohdanLeontovych <bogdanleontovich@gmail.com>
2024-06-06 15:25:22 +0200

    footer title and hover

--- app/academy/sections/Footer/index.tsx
+++ app/academy/sections/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss'
 import Button from '@/components/Button'
 
 import { useLanguage } from '@/contexts/LanguageContext'
-import useScreenModeAndSize from '@/hooks/useScreenModeAndSize'
+// import useScreenModeAndSize from '@/hooks/useScreenModeAndSize'
 
 import { icons } from './icons'
 import Divider from '@/components/Divider'
@@ -14,24 +14,26 @@ import { forwardRef } from 'react'
 
 const Footer = forwardRef<HTMLDivElement>(function (_, ref) {
   const { lang } = useLanguage()
-  const { width } = useScreenModeAndSize()
+  // const { width } = useScreenModeAndSize()
 
   return (
     <footer ref={ref} id={AcademyIDs.Footer} className={styles.footer}>
       <div className={styles.footerTop}>
         <div className={styles.footerTopContent}>
           <div className={styles.right}>
-            {width < 800
+            {/* {width < 800
               ? icons.footerLogo.mobile[lang](styles.footerLogo)
-              : icons.footerLogo.desktop[lang](styles.footerLogo)}
+              : icons.footerLogo.desktop[lang](styles.footerLogo)} */}
+            {icons.footerLogo.mobile[lang](styles.footerLogo)}
 
             <div className={styles.buttonGroup}>
               <Button as="link" variant="primary-white" href="/" className={styles.button}>
                 Subscribe
               </Button>
-              <Button as="link" variant="secondary-fill-black" href="/" className={styles.button}>
+              {/* <Button as="link" variant="secondary-fill-black" href="/" className={styles.button}>
                 Support Academy
-              </Button>
+              </Button> */}
+              <button className={styles.supportAcademyButton}>Support Academy</button>
             </div>
           </div>
           <div className={styles.left}>
